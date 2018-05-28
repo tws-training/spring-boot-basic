580cf18c110259c3b6455bc78f63c2eac8d7a126
linwenjun <linwenjun@163.com>
2018-05-29 06:04:56 +0800

    fix dir error

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 ssh -i $keyfile ubuntu@$WebServer 'mkdir -p /home/ubuntu/works/$NameSpace'
+ssh -i $keyfile ubuntu@$WebServer 'ls -la /home/ubuntu/works/$NameSpace'
 scp -i $keyfile ./build/libs/demo-*.jar ubuntu@$WebServer:/home/ubuntu/works/$NameSpace/demo.jar
 ssh -i $keyfile ubuntu@$WebServer 'ls /home/ubuntu/works/$NameSpace'
 ssh -i $keyfile ubuntu@$WebServer 'kill -9 \$(lsof -t -i:\$port) 2>&1 &'
