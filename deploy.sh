b273aa96572a5eb250638eb051f4ad62d703903e
linwenjun <linwenjun@163.com>
2018-05-29 06:30:58 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ ssh -i $keyfile ubuntu@$WebServer mkdir -p /home/ubuntu/works/$NameSpace
 ssh -i $keyfile ubuntu@$WebServer ls /home/ubuntu/works
 scp -i $keyfile ./build/libs/demo-*.jar ubuntu@$WebServer:/home/ubuntu/works/$NameSpace/demo.jar
 ssh -i $keyfile ubuntu@$WebServer ls /home/ubuntu/works/$NameSpace
-ssh -i $keyfile ubuntu@$WebServer kill -9 $(lsof -t -i:$port) 2>&1 &
-ssh -i $keyfile ubuntu@$WebServer java -jar /home/ubuntu/works/$NameSpace/demo.jar > demo.log
+ssh -i $keyfile ubuntu@$WebServer 'kill -9 $(lsof -t -i:$port) 2>&1 &'
+ssh -i $keyfile ubuntu@$WebServer nohup java -jar /home/ubuntu/works/$NameSpace/demo.jar > demo.log
